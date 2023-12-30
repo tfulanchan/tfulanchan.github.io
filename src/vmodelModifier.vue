e798cf7abcd8c5b28a406562147418bedec869cd
tfulanchan <h951115951115@gmail.com>
2023-12-30 14:23:36 +0300

    Updates

--- src/vmodelModifier.vue
+++
@@ -1,8 +0,0 @@
-<script setup></script>
-
-<template>
-  <div>
-      <a href="https://vuejs.org/guide/components/v-model.html#handling-v-model-modifiers">Handling v-model modifiers</a>
-      <p>Modifiers added to a component v-model can be accessed in the child component by destructuring the defineModel() return value</p>
-  </div>
-</template>
