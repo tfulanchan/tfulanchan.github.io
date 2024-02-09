d577000c1fdc4818eed32f020c19b571cc74cb76
tfulanchan <60479975+tfulanchan@users.noreply.github.com>
2024-02-09 22:01:49 +0300

    Create onunmounted.vue
    https://twitter.com/danielkelly_io/status/1756011382267429246

---
+++ src/onunmounted.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref, onUnmounted } from 'vue';
+const crossed = ref(false);
+
+const int = setInterval(() => {
+  crossed.value = !crossed.value;
+}, 500);
+
+onUnmounted(() => {
+  clearInterval(int);
+});
+</script>
+
+<template>
+  <h1 style="font-size: 10rem">
+    {{ crossed ? '🤞' : '✌️' }}
+  </h1>
+</template>
